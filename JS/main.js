71906404110013f3c5f54b5e9a45ecf85bdf5e08
AnujaAnil123 <135549861+AnujaAnil123@users.noreply.github.com>
2023-06-13 17:01:08 +0400

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,17 @@
+let  nav=document.querySelector(".navbar");
+window.onscroll = function(){
+    if(document.documentElement.scrollTop>50){
+        nav.classList.add("header-scrolled");
+    }else{nav.classList.remove("header-scrolled");
+
+    }
+}
+let navbar = document.querySelectorAll(".nav-link");
+let navcollapse = document.querySelector(".navbar-cpllapse.collapse");
+navbar.forEach(function(e){
+    e.addEventListener("click", function(){
+
+   
+    navcollapse.classList.remove("show");
+})
+})
